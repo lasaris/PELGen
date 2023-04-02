a5934d147d9bd7ad34966e23e3af363444d9c276
Daniel Rozehnal <514184@mail.muni.cz>
2023-04-02 21:04:51 +0200

    feat: add file manager logic

--- EventLogGenerator/EventLogGenerator/InputOutput/FileManager.cs
+++ EventLogGenerator/EventLogGenerator/InputOutput/FileManager.cs
@@ -5,15 +5,63 @@ public static class FileManager
     public static string OutputFileName = "output.csv";
 
     public static string OutputFolderName = "generated";
-    
-    public static void AppendLine(string line, string? filename = null)
+
+    /// <summary>
+    /// Creates new CSV file and appends given header to it
+    /// </summary>
+    /// <param name="headerLine">header for CSV file</param>
+    /// <param name="filename">name of newly created file</param>
+    public static void SetupNewCsvFile(string headerLine, string? filename = null)
     {
         filename ??= OutputFileName;
-        
-        string outputPath = Path.Combine(OutputFolderName, filename);
         Directory.CreateDirectory(OutputFolderName);
+        AppendLine(headerLine);
+    }
 
-        using StreamWriter writer = new StreamWriter(outputPath, true);
+    /// <summary>
+    /// Safely tries to append line to already created CSV file.
+    /// </summary>
+    /// <param name="line">valid CSV line of data for given file</param>
+    /// <param name="filename">name of file that line should be appended to</param>
+    /// <exception cref="ArgumentException">Thrown on invalid filename, file format or line data format</exception>
+    public static void AppendLineToCsv(string line, string? filename = null)
+    {
+        filename ??= OutputFileName;
+        string outPath = Path.Combine(OutputFolderName, filename);
+
+        if (!File.Exists(outPath))
+        {
+            throw new ArgumentException("Provided file does not exist");
+        }
+
+        using var reader = new StreamReader(outPath);
+        var headerLine = reader.ReadLine();
+        if (headerLine == null)
+        {
+            throw new ArgumentException("Given CSV file has no header");
+        }
+        
+        var columnCount = headerLine.Split(',').Length;
+        var lineColumnCount = line.Split(',').Length;
+
+        if (columnCount != lineColumnCount)
+        {
+            throw new ArgumentException($"Given CSV file has {columnCount} columns, but in file there were {lineColumnCount} given");
+        }
+        
+        AppendLine(line, filename);
+    }
+    
+    private static void AppendLine(string line, string? filename = null)
+    {
+        filename ??= OutputFileName;
+        string outPath = Path.Combine(OutputFolderName, filename);
+        if (!File.Exists(outPath))
+        {
+            File.Create(outPath);
+        }
+        
+        using StreamWriter writer = new StreamWriter(outPath, true);
         writer.WriteLine(line);
     }
 }
