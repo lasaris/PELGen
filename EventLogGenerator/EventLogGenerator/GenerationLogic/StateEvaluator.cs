dc05fa79cbad4bdb74409197d7936a93b558d11e
Daniel Rozehnal <514184@mail.muni.cz>
2023-04-07 16:02:43 +0200

    fix: compilation and refactor logging messages

--- EventLogGenerator/EventLogGenerator/GenerationLogic/StateEvaluator.cs
+++ EventLogGenerator/EventLogGenerator/GenerationLogic/StateEvaluator.cs
@@ -20,13 +20,10 @@ public static class StateEvaluator
     // All current available states of the process
     public static HashSet<ProcessState> AllStates = new();
 
-    // ProcessStates that are available for next step in the current process
-    public static HashSet<ProcessState> AvailableNext = new();
-
     // The time when process has to be finished. Can be used as emergency limit
     public static DateTime ProcessEnd { get; set; }
-    
-    private static readonly Random random = new Random();
+
+    private static readonly Random random = new();
 
     public static void AddState(ProcessState state)
     {
@@ -43,15 +40,13 @@ public static class StateEvaluator
         {
             throw new Exception("ActorFrame must be set before running the evaluator");
         }
- 
-        Console.WriteLine("[INFO] Process run started");
+
+        Console.WriteLine("[INFO] --- PROCESS RUN STARTED---");
         // Running loop
         while (!CurrentActorFrame.CurrentState.IsFinishing)
         {
-            Console.WriteLine($"[INFO] Current state {CurrentActorFrame.CurrentState.ActivityType} - {CurrentActorFrame.CurrentState.Resource}");
-            // reset AvailableNext
-            AvailableNext = new();
-
+            Console.WriteLine(
+                $"[INFO] Current state {CurrentActorFrame.CurrentState.ActivityType} - {CurrentActorFrame.CurrentState.Resource}");
             // Create copy of all states
             HashSet<ProcessState> allStatesCopy = new HashSet<ProcessState>(AllStates);
 
@@ -112,9 +107,8 @@ public static class StateEvaluator
                 CurrentActorFrame.CurrentTime = CurrentActorFrame.CurrentTime.AddMinutes(5);
                 if (CurrentActorFrame.CurrentTime >= ProcessEnd)
                 {
-                    Console.WriteLine(
-                        $"Current tim reached the end time of the process in state {currentState.ActivityType} {currentState.Resource}");
-                    return;
+                    Console.WriteLine($"[INFO] Current time reached the end time process end {currentState.ActivityType} {currentState.Resource}");
+                    break;
                 }
             }
 
@@ -124,73 +118,94 @@ public static class StateEvaluator
             foreach (var state in allStatesCopy)
             {
                 float rating = 0;
-                
+
                 // Rank compulsory
                 if (state.Rules.IsCompulsory)
                 {
                     rating += Constants.CompulsoryWeight;
                 }
-                
+
                 // Rank following activities
                 if (currentState.Rules.FollowingActivitiesMap != null
                     && currentState.Rules.FollowingActivitiesMap.Keys.Contains(state.ActivityType))
                 {
-                    rating += currentState.Rules.FollowingActivitiesMap[state.ActivityType] * Constants.ChanceToFollowWeight;
+                    rating += currentState.Rules.FollowingActivitiesMap[state.ActivityType] *
+                              Constants.ChanceToFollowWeight;
                 }
 
                 // Rank chance to visit
                 rating += state.Chances.ChanceToVisit * Constants.ChanceToVisitWeight;
-                
+
                 // Rank activity chance
                 if (currentState.ActivityType != state.ActivityType)
                 {
                     rating += Constants.DifferentActivityWeight;
                 }
-                
+
                 // Penalize previous visit
                 rating += CurrentActorFrame.VisitedMap[state] * Constants.EachPreviousVisitWeight;
-                
+
                 // Penalize last visited
                 if (CurrentActorFrame.LastVisited != null && CurrentActorFrame.LastVisited.Equals(state))
                 {
                     rating += Constants.LastVisitWeight;
                 }
-                
+
                 // Rank same resource
                 if (currentState.Resource == state.Resource)
                 {
                     rating += Constants.SameResourceWeight;
                 }
-                
+
                 // Rank finishing state
                 if (state.IsFinishing)
                 {
                     rating += Constants.ToFinishingWeight;
                 }
-                
+
                 weightedStates.Add(state, rating);
             }
 
             // Handle empty states
             if (!weightedStates.Any())
             {
+                Console.WriteLine("[INFO] No states available, looping and adding time");
                 continue;
             }
 
             var nextState = SelectWeightedState(weightedStates);
             JumpNextState(nextState, nextState.TimeFrame.PickTimeByDistribution(CurrentActorFrame.CurrentTime));
         }
+        Console.Out.WriteLine("[INFO] --- ENDING PROCESS RUN ---");
     }
 
     private static void JumpNextState(ProcessState newState, DateTime jumpDate)
     {
-        // TODO: change current state and time, number of current loops if current == newState
+        // Update VisitedMap
+        if (CurrentActorFrame.VisitedMap.Keys.Contains(CurrentActorFrame.CurrentState))
+        {
+            CurrentActorFrame.VisitedMap[CurrentActorFrame.CurrentState] += 1;
+        }
+        else
+        {
+            CurrentActorFrame.VisitedMap.Add(newState, 1);
+        }
 
-        // TODO: update AvailableNext
+        // Update loop count or current state to the new one
+        if (CurrentActorFrame.CurrentState.Equals(newState))
+        {
+            CurrentActorFrame.CurrentLoopCount += 1;
+        }
+        else
+        {
+            CurrentActorFrame.LastVisited = CurrentActorFrame.CurrentState;
+            CurrentActorFrame.CurrentState = newState;    
+        }
 
+        CurrentActorFrame.CurrentTime = jumpDate;
         OnStateEnter(CurrentActorFrame.Actor, newState, jumpDate);
     }
-    
+
     public static ProcessState SelectWeightedState(Dictionary<ProcessState, float> stateWeights)
     {
         float totalWeight = stateWeights.Values.Sum();
