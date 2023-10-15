2a2a352512e50b836fd34822681856b5488b0f40
Daniel Rozehnal <514184@mail.muni.cz>
2023-10-15 15:30:39 +0200

    feat: prepare test suites

--- EventLogGenerator/UnitTests/UnitTest1.cs
+++ EventLogGenerator/UnitTests/UnitTest1.cs
@@ -1,14 +1,86 @@
+using EventLogGenerationLibrary;
+
 namespace UnitTests;
 
-public class Tests
+/// <summary>
+/// Tests that all necessary objects are accessible and have the expected parameters.
+/// </summary>
+[TestFixture]
+public class ObjectsFixture
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public void CreateConfiguration()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void CreateProcessStates()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void CreateSprinkleStates()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void CreateOtherStates()
+    {
+        Assert.Pass();
+    }
+}
+
+/// <summary>
+/// Tests very simple process runs only using ProcessStates.
+/// </summary>
+[TestFixture]
+public class SimpleProcessFixture
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.Pass();
+    }
+}
+
+/// <summary>
+/// Tests simple process while using SprinkleService and its features with different SprinkleStates.
+/// </summary>
+[TestFixture]
+public class ProcessWithSprinklesFixture
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.Pass();
+    }
+}
+
+/// <summary>
+/// Takes all the examples from the Examples project and checks whether they are correctly generated. 
+/// </summary>
+[TestFixture]
+public class ExampleCasesFixture
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.Pass();
+    }
+}
+
+/// <summary>
+/// This is a complex fixture that uses most of the features from the PELGen.
+/// It tests that the InformationSystemGeneration example project was generated correctly.
+/// </summary>
+[TestFixture]
+public class InformationSystemGenerationFixture
+{
+    [Test]
+    public void SampleTest()
     {
         Assert.Pass();
     }
