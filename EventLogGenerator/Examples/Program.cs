85a98e66425fb76596a3980b83b6076c5767faab
Daniel Rozehnal <514184@mail.muni.cz>
2023-10-16 17:22:00 +0200

    feat: add most of the examples for library usage

--- EventLogGenerator/Examples/Program.cs
+++ EventLogGenerator/Examples/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EventLogGenerationLibrary;
 using EventLogGenerationLibrary.Models.States;
 using EventLogGenerator.Models;
+using EventLogGenerator.Services;
 
 namespace Examples;
 
@@ -8,7 +9,7 @@ namespace Examples;
 /// This project contains multiple examples of the library usage.
 /// More complex example is in InformationSystemGeneration project.
 /// </summary>
-internal class Program
+public class Program
 {
     /// <summary>
     /// Firstly we try to model a process with 3 single activities following each other in a strict path and exact time.
@@ -113,19 +114,95 @@ internal class Program
     }
 
     /// <summary>
-    /// Here we add sprinkles, which mimic random events and are added randomly for a given timeframe and distribution.
+    /// Here we add sprinkles, which mimic random events and are added randomly in between specified process states.
     /// </summary>
     private static void ProcessWithSprinkles()
     {
-        // TODO
+        // Define common resources
+        var resource = "IT Project";
+
+        // Create states with exact time of execution
+        var state1 = new ProcessState("Requirement gathering", resource, 1, new DateTime(2023, 8, 10, 10, 00, 00));
+        var state2 = new ProcessState("Development start", resource, 1, new DateTime(2023, 8, 14, 08, 00, 00));
+        var state3 = new ProcessState("Acceptance testing", resource, 1, new DateTime(2023, 10, 10, 13, 00, 00), true);
+
+        // Add following states to model the process
+        state1.AddFollowingState(state2);
+        state2.AddFollowingState(state3);
+
+        // Add random sprinkles (meetings) to the process
+        // Hashmaps defines start and end states that sprinkle should be randomly added in between
+        var planningMeeeting = new SprinkleState("Planning team meet", "Google Meet",
+            new HashSet<ProcessState>() { state1 },
+            new HashSet<ProcessState>() { state2 });
+        // By adding numerical "passes" parameter, the sprinkle will be randomly added that specified amount of times (here it's 4)
+        var qualityMeeting = new SprinkleState("QA team meeting", "Zoom",
+            new HashSet<ProcessState>() { state2 },
+            new HashSet<ProcessState>() { state3 }, null, null, null, 4);
+
+        // Create configuration
+        var config = new Configuration(1, state1, 1, null, "project.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Company");
+
+        // Create generator and run generation
+        var generator = new EventGenerator(config);
+        generator.RunGeneration();
     }
 
     /// <summary>
-    /// This example shows all currently available kinds of sprinkle states.
+    /// This example uses and describes all other available states to be used.
     /// </summary>
-    private static void ProcessWithAllSprinkles()
+    private static void ProcessWithMoreStates()
     {
-        // TODO
+        // Define common resources
+        var resource = "Support Software";
+
+        // Create states with exact time of execution
+        var state1 = new ProcessState("Issue reported", resource, 1, new DateTime(2023, 8, 10, 22, 00, 00));
+        var state2 = new ProcessState("Analysis performed", resource, 1, new DateTime(2023, 8, 14, 08, 00, 00));
+        var state3 = new ProcessState("Resolution added", resource, 1, new DateTime(2023, 8, 29, 16, 00, 00));
+        var state4 = new ProcessState("Feedback gathered", resource, 1, new DateTime(2023, 9, 2, 13, 30, 00), true);
+
+        // Add following states to model the process
+        state1.AddFollowingStates((state2, 0.6f), (state3, 0.4f));
+        state2.AddFollowingState(state3);
+        state3.AddFollowingState(state4);
+
+        // Dynamic Sprinkle state
+        // Triggered by specified states and then added randomly in between the state it reacts to and offset based on the distribution
+        var dynamicSprinkle = new DynamicSprinkleState("Notify analyst", "Emailing service",
+            new HashSet<ProcessState>() { state1 }, TimeSpan.FromHours(1), ETimeFrameDistribution.Linear);
+
+        // Conditional Sprinkle
+        // Only added if some state is present in the process trace with some fixed time offset after it
+        var conditionalSprinkle = new ConditionalSprinkle(state2, new DummyState("Review analysis", "Jira"),
+            null,
+            TimeSpan.FromDays(2));
+
+        // Fixed time state
+        // This state occur at a fixed time but are not necessarily part of the process run
+        var fixedState =
+            new FixedTimeState("Update due date", "Time cron", new DateTime(2023, 9, 01));
+
+        // Interval Sprinkle state
+        // Occurs randomly once in the specified timeframe
+        var intervalSprinkle = new IntervalSprinkleState("Quality assurance", "Testing SW",
+            new TimeFrame(new DateTime(2023, 8, 15, 08, 00, 00), new DateTime(2023, 9, 2, 13, 30, 00)));
+
+        // Periodic Sprinkle state
+        // Runs in given interval between process states with a given period (as much as it fits with that period)
+        var periodicSprinkle = new PeriodicSprinkleState("Tickets backup", "Database service",
+            new HashSet<ProcessState>() { state1 }, new HashSet<ProcessState>() { state4 }, TimeSpan.FromDays(7));
+
+        // Create configuration
+        var config = new Configuration(30, state1, 1, null, "support.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Ticket");
+
+        // Create generator and run generation
+        var generator = new EventGenerator(config);
+        generator.RunGeneration();
     }
 
     /// <summary>
@@ -133,7 +210,7 @@ internal class Program
     /// </summary>
     private static void DependantProcess()
     {
-        // TODO
+        // TODO - better process dependency needs to be created
     }
 
     /// <summary>
@@ -141,10 +218,14 @@ internal class Program
     /// </summary>
     public static void Main()
     {
+        // Setting seed makes sure when we run the process generation, we always get the same results (can be omitted)
+        RandomService.SetSeed(4269123);
+
         SingleTraceProcess();
         SingleTraceProcessRandomTimes();
         ProcessWithBranching();
         ProcessWithSprinkles();
-        ProcessWithAllSprinkles();
+        ProcessWithMoreStates();
+        DependantProcess();
     }
 }
