888f665c653f1457a4b95385ba41628326106e0b
Daniel Rozehnal <514184@mail.muni.cz>
2023-10-20 11:50:11 +0200

    feat: add fully functional unit tests

--- UnitTests/TestUtils.cs
+++ UnitTests/TestUtils.cs
@@ -1,6 +1,21 @@
-﻿namespace UnitTests;
+﻿using EventLogGenerator.Models;
+using Newtonsoft.Json;
 
-public class TestUtils
+namespace UnitTests;
+
+public static class TestUtils
 {
-    
+    public static void CompareWithProcessFixture(Process process, string fileName)
+    {
+        string generated = JsonConvert.SerializeObject(process, Formatting.Indented);
+        string fixture = File.ReadAllText("../../../../UnitTests/Fixtures/Examples/" + fileName);
+        Assert.True(String.Equals(generated, fixture));
+    }
+
+    public static void CompareFiles(string path1, string path2)
+    {
+        string content1 = File.ReadAllText(path1);
+        string content2 = File.ReadAllText(path2);
+        Assert.True(string.Equals(content1, content2, StringComparison.OrdinalIgnoreCase));
+    }
 }
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,87 +1,215 @@
+using System.Reflection;
 using EventLogGenerationLibrary;
+using EventLogGenerationLibrary.Models.States;
+using EventLogGenerator.Models;
+using EventLogGenerator.Services;
+using Newtonsoft.Json;
 
 namespace UnitTests;
 
 /// <summary>
-/// Tests that all necessary objects are accessible and have the expected parameters.
+/// Test that examples are generated correctly. The logic behind generation should not change over time.
 /// </summary>
 [TestFixture]
-public class ObjectsFixture
+public class TestGeneratedProcessesFixture
 {
+    public string PathToExamples = "../../../../UnitTests/Fixtures/Examples/";
+
     [Test]
-    public void CreateConfiguration()
+    public void SingleTraceProcessTest()
     {
-        Assert.Pass();
+        RandomService.SetSeed(4269123);
+
+        var resource = "Assembly line 42";
+        var state1 = new ProcessState("Component assembly", resource, 1, new DateTime(2023, 10, 13, 12, 00, 00));
+        var state2 = new ProcessState("Quality inspection", resource, 1, new DateTime(2023, 10, 13, 12, 00, 10));
+        var state3 = new ProcessState("Product packing", resource, 1, new DateTime(2023, 10, 13, 13, 30, 40), true);
+
+        state1.AddFollowingState(state2);
+        state2.AddFollowingState(state3);
+
+        var config = new Configuration(1, state1, 1, null, "manufacture.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Worker");
+
+        var generator = new EventGenerator(config);
+        var process = generator.RunGeneration();
+
+        TestUtils.CompareWithProcessFixture(process, "single-trace.json");
+        TestUtils.CompareFiles(PathToExamples + "manufacture.csv", "./generated/manufacture.csv");
+    }
+
+    [Test]
+    public void SingleTraceProcessRandomTimesTest()
+    {
+        RandomService.SetSeed(4269123);
+
+        var resource = "Morning routine";
+        var state1 = new ProcessState("Wake up", resource, 1, new DateTime(2023, 10, 27, 7, 00, 00));
+        var state2 = new ProcessState("Take shower", resource, 1,
+            new TimeFrame(new DateTime(2023, 10, 27, 7, 00, 00), new DateTime(2023, 10, 27, 7, 30, 00)));
+        var state3 = new ProcessState("Prepare breakfast", resource, 1,
+            new TimeFrame(new DateTime(2023, 10, 27, 7, 30, 00), new DateTime(2023, 10, 27, 8, 00, 00),
+                ETimeFrameDistribution.ReverseExponential), true);
+
+        state1.AddFollowingState(state2);
+        state2.AddFollowingState(state3);
+
+        var config = new Configuration(1, state1, 1, null, "routine.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Human");
+
+        var generator = new EventGenerator(config);
+        var process = generator.RunGeneration();
+        
+        TestUtils.CompareWithProcessFixture(process, "single-trace-random.json");
+        TestUtils.CompareFiles(PathToExamples + "routine.csv", "./generated/routine.csv");
     }
     
     [Test]
-    public void CreateProcessStates()
+    public void ProcessWithBranchingTest()
     {
-        Assert.Pass();
+        RandomService.SetSeed(4269123);
+
+        var quiz = "Test quiz";
+        var question1 = "Question 1";
+        var question2 = "Question 2";
+        var openQuiz = new ProcessState("Open quiz", quiz, 1, new DateTime(2023, 9, 23, 12, 00, 00));
+        var answerYes = new ProcessState("Yes answer", question1, 1,
+            new TimeFrame(new DateTime(2023, 9, 23, 12, 00, 00), new DateTime(2023, 9, 23, 12, 05, 00)));
+        var answerNo = new ProcessState("No answer", question1, 1,
+            new TimeFrame(new DateTime(2023, 9, 23, 12, 00, 00), new DateTime(2023, 9, 23, 12, 05, 00)));
+        var answerTrue = new ProcessState("True answer", question2, 1,
+            new TimeFrame(new DateTime(2023, 9, 23, 12, 05, 00), new DateTime(2023, 9, 23, 12, 10, 00)));
+        var answerFalse = new ProcessState("False answer", question2, 1,
+            new TimeFrame(new DateTime(2023, 9, 23, 12, 05, 00), new DateTime(2023, 9, 23, 12, 10, 00)));
+        var submitQuiz = new ProcessState("Submit quiz", quiz, 1,
+            new TimeFrame(new DateTime(2023, 9, 23, 12, 10, 00), new DateTime(2023, 9, 23, 12, 12, 00)), true);
+
+        openQuiz.AddFollowingStates((answerYes, 0.8f), (answerNo, 0.2f));
+        answerYes.AddFollowingStates((answerTrue, 0.3f), (answerFalse, 0.7f));
+        answerNo.AddFollowingStates((answerTrue, 0.5f), (answerFalse, 0.5f));
+        answerTrue.AddFollowingState(submitQuiz);
+        answerFalse.AddFollowingState(submitQuiz);
+
+        var config = new Configuration(30, openQuiz, 1, null, "quiz.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Student");
+
+        var generator = new EventGenerator(config);
+        var process = generator.RunGeneration();
+        
+        TestUtils.CompareWithProcessFixture(process, "branching.json");
+        TestUtils.CompareFiles(PathToExamples + "quiz.csv", "./generated/quiz.csv");
     }
     
     [Test]
-    public void CreateSprinkleStates()
+    public void ProcessWithSprinklesTest()
     {
-        Assert.Pass();
+        RandomService.SetSeed(4269123);
+
+        var resource = "IT Project";
+        var state1 = new ProcessState("Requirement gathering", resource, 1, new DateTime(2023, 8, 10, 10, 00, 00));
+        var state2 = new ProcessState("Development start", resource, 1, new DateTime(2023, 8, 14, 08, 00, 00));
+        var state3 = new ProcessState("Acceptance testing", resource, 1, new DateTime(2023, 10, 10, 13, 00, 00), true);
+
+        state1.AddFollowingState(state2);
+        state2.AddFollowingState(state3);
+
+        var planningMeeeting = new SprinkleState("Planning team meet", "Google Meet",
+            new HashSet<ProcessState>() { state1 },
+            new HashSet<ProcessState>() { state2 });
+        var qualityMeeting = new SprinkleState("QA team meeting", "Zoom",
+            new HashSet<ProcessState>() { state2 },
+            new HashSet<ProcessState>() { state3 }, null, null, null, 4);
+
+        var config = new Configuration(1, state1, 1, null, "project.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Company");
+
+        var generator = new EventGenerator(config);
+        var process = generator.RunGeneration();
+        
+        TestUtils.CompareWithProcessFixture(process, "sprinkles.json");
+        TestUtils.CompareFiles(PathToExamples + "project.csv", "./generated/project.csv");
     }
     
     [Test]
-    public void CreateOtherStates()
+    public void ProcessWithMoreStatesTest()
     {
-        Assert.Pass();
-    }
-}
+        RandomService.SetSeed(4269123);
 
-/// <summary>
-/// Tests very simple process runs only using ProcessStates.
-/// </summary>
-[TestFixture]
-public class SimpleProcessFixture
-{
-    [Test]
-    public void SampleTest()
-    {
-        Assert.Pass();
-    }
-}
+        var resource = "Support Software";
+        var state1 = new ProcessState("Issue reported", resource, 1, new DateTime(2023, 8, 10, 22, 00, 00));
+        var state2 = new ProcessState("Analysis performed", resource, 1, new DateTime(2023, 8, 14, 08, 00, 00));
+        var state3 = new ProcessState("Resolution added", resource, 1, new DateTime(2023, 8, 29, 16, 00, 00));
+        var state4 = new ProcessState("Feedback gathered", resource, 1, new DateTime(2023, 9, 2, 13, 30, 00), true);
 
-/// <summary>
-/// Tests simple process while using SprinkleService and its features with different SprinkleStates.
-/// </summary>
-[TestFixture]
-public class ProcessWithSprinklesFixture
-{
-    [Test]
-    public void SampleTest()
-    {
-        Assert.Pass();
-    }
-}
+        state1.AddFollowingStates((state2, 0.6f), (state3, 0.4f));
+        state2.AddFollowingState(state3);
+        state3.AddFollowingState(state4);
 
-/// <summary>
-/// Takes all the examples from the Examples project and checks whether they are correctly generated. 
-/// </summary>
-[TestFixture]
-public class ExampleCasesFixture
-{
-    [Test]
-    public void SampleTest()
-    {
-        Assert.Pass();
-    }
-}
+        var dynamicSprinkle = new DynamicSprinkleState("Notify analyst", "Emailing service",
+            new HashSet<ProcessState>() { state1 }, TimeSpan.FromHours(1), ETimeFrameDistribution.Linear);
 
-/// <summary>
-/// This is a complex fixture that uses most of the features from the PELGen.
-/// It tests that the InformationSystemGeneration example project was generated correctly.
-/// </summary>
-[TestFixture]
-public class InformationSystemGenerationFixture
-{
+        var conditionalSprinkle = new ConditionalSprinkle(state2, new DummyState("Review analysis", "Jira"),
+            null,
+            TimeSpan.FromDays(2));
+
+        var fixedState =
+            new FixedTimeState("Update due date", "Time cron", new DateTime(2023, 9, 01));
+
+        var intervalSprinkle = new IntervalSprinkleState("Quality assurance", "Testing SW",
+            new TimeFrame(new DateTime(2023, 8, 15, 08, 00, 00), new DateTime(2023, 9, 2, 13, 30, 00)));
+
+        var periodicSprinkle = new PeriodicSprinkleState("Tickets backup", "Database service",
+            new HashSet<ProcessState>() { state1 }, new HashSet<ProcessState>() { state4 }, TimeSpan.FromDays(7));
+
+        var config = new Configuration(30, state1, 1, null, "support.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Ticket");
+
+        var generator = new EventGenerator(config);
+        var process = generator.RunGeneration();
+        
+        TestUtils.CompareWithProcessFixture(process, "more-states.json");
+        TestUtils.CompareFiles(PathToExamples + "support.csv", "./generated/support.csv");
+    }
+    
     [Test]
-    public void SampleTest()
+    public void DependantProcessTest()
     {
-        Assert.Pass();
+        RandomService.SetSeed(4269123);
+
+        var resource = "Assembly line 42";
+        var state1 = new ProcessState("Component assembly", resource, 1, new DateTime(2023, 10, 13, 12, 00, 00));
+        var state2 = new ProcessState("Quality inspection", resource, 1, new DateTime(2023, 10, 13, 12, 00, 10));
+        var state3 = new ProcessState("Product packing", resource, 1, new DateTime(2023, 10, 13, 13, 30, 40), true);
+        state1.AddFollowingState(state2);
+        state2.AddFollowingState(state3);
+        var config = new Configuration(1, state1, 1, null, "reactive-manufacture.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Suspected worker");
+        var generator = new EventGenerator(config);
+        var previousProcess = generator.RunGeneration();
+        var newState1 = new ProcessState("Begin daily inspection", "Management System", 1, new DateTime(2023, 10, 13, 8, 0, 0),
+            true);
+        
+        var reactiveState1 = new ReactiveState("Check assembly", "placeholder", "Component assembly");
+        var reactiveState2 = new ReactiveState("Check quality inspection", "placeholder", "Quality inspection", null,
+            "Quality management system", TimeSpan.FromMinutes(5));
+        var patternReaction = new PatternReaction(new List<string>(){ "Quality inspection", "Product packing" },
+            "Product packing", "Check packed product");
+
+        var newConfig = new Configuration(1, newState1, 1, null, "reactive-manager.csv",
+            "ActorId,ActorType,Activity,Resource,Timestamp",
+            "Manager", previousProcess);
+        
+        // Generate process with reactive states
+        var newGenerator = new EventGenerator(newConfig);
+        var process =newGenerator.RunGeneration();
+        
+        TestUtils.CompareWithProcessFixture(process, "reactive-process.json");
+        TestUtils.CompareFiles(PathToExamples + "reactive-manager.csv", "./generated/reactive-manager.csv");
     }
 }
